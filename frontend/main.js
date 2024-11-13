1c10aa5de12ac48821704ac778b9ddd356780a40
Altbegz <shmuelbisberg.acc@gmail.com>
2024-11-13 22:31:16 +0200

    Add initial data and endpoints configuration for categories and subscription

---
+++ frontend/main.js
@@ -0,0 +1,22 @@
+import "./style.css";
+import Alpine from "alpinejs";
+import endpoints from "./data/endpoints.json";
+
+window.Alpine = Alpine;
+
+console.log(endpoints.categories)
+
+// Store for global endpoints
+Alpine.store("endpoints", {
+  categoriesEndpoint: import.meta.env.DEV
+    ? endpoints.categories.dev
+    : endpoints.categories.prod,
+  subscribeEndpoint: import.meta.env.DEV
+    ? endpoints.subscribe.dev
+    : endpoints.subscribe.prod,
+  unsubscribeEndpoint: import.meta.env.DEV
+    ? endpoints.unsubscribe.dev
+    : endpoints.unsubscribe.prod,
+});
+
+Alpine.start();
